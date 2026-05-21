f634ddf43637f0bf1247d94b339153d3a2c0e294
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-05-21 17:07:28 +0600

    Remove translations from Navbar component

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,12 +1,10 @@
 "use client";
 import Navbar from "@/Components/Navbar/Navbar";
-import { useTranslations } from "next-intl";
 
 export default function ClientPageLayout({ children }) {
-  const translations = useTranslations();
   return (
     <section>
-      <Navbar translations={translations.Navbar} />
+      <Navbar />
       {children}
     </section>
   );
